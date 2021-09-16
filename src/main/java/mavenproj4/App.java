cea885da00435139e49fa0dfbf0bb5d169191ce2
aangsiety <anandudileep07@gmail.com>
2021-09-16 10:23:31 +0530

    Initial commit

---
+++ src/main/java/mavenproj4/App.java
@@ -0,0 +1,13 @@
+package mavenproj4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
